03792aafbf565ad4780628fd19c4997af8994f57
Glenn Lanzer <lanzertrey@gmail.com>
2018-10-20 14:34:18 -0500

    Add: fcc tut render a class component to the dom

---
+++ tutorials/freeCodeCamp/03-front-end-libraries/render-a-class-component-to-the-dom/components/Fruits.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Fruits = () => {
+    return (
+        <div>
+            <h2>Fruits:</h2>
+            <ul>
+                <li>Apples</li>
+                <li>Oranges</li>
+                <li>Bananas</li>
+                <li>Peaches</li>
+            </ul>
+        </div>
+    );
+};
+
+export default App
---
+++ tutorials/freeCodeCamp/03-front-end-libraries/render-a-class-component-to-the-dom/components/Vegetables.js
@@ -0,0 +1,16 @@
+import Vegetables from './Vegetables'
+import Fruits from './Fruits'
+
+const Vegetables = () => {
+    return (
+        <div>
+            <h2>Types of Vegetables:</h2>
+            <ul>
+                <li>Carrots</li>
+                <li>Lettuce</li>
+                <li>Broccolli</li>
+                <li>Mushrooms</li>
+            </ul>
+        </div>
+    );
+}
