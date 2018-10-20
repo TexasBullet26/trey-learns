c0f4a4f3435b613351f0bb7d196b31d1010dae2b
Glenn Lanzer <lanzertrey@gmail.com>
2018-10-20 14:35:17 -0500

    Add: fcc tut compose-react-components.js

---
+++ tutorials/freeCodeCamp/03-front-end-libraries/04-react/compose-react-components/compose-react-components.js
@@ -0,0 +1,67 @@
+const NonCitrus = () => {
+    render(
+        <div>
+            <ul>
+                <li>Apples</li>
+                <li>Blueberries</li>
+                <li>Strawberries</li>
+                <li>Bananas</li>
+            </ul>
+        </div>
+    );
+}
+
+const Citrus = () => {
+    render(
+        <div>
+            <ul>
+                <li>Lemon</li>
+                <li>Lime</li>
+                <li>Orange</li>
+                <li>Grapefruit</li>
+            </ul>
+        </div>
+    );
+}
+
+const Vegetables = () => {
+    render(
+        <div>
+            <ul>
+                <li>Brussel Sprouts</li>
+                <li>Broccoli</li>
+                <li>Squash</li>
+            </ul>
+        </div>
+    );
+}
+
+class Fruits extends ReactComponent {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h2>Fruits:</h2>
+                <NonCitrus/>
+                <Citrus/>
+            </div>
+        );
+    }
+};
+
+class TypesOfFood extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>Types of Food:</h1>
+                <Fruits/>
+                <Vegetables/>
+            </div>
+        );
+    }
+};
