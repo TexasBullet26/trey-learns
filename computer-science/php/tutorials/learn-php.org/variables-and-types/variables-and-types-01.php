801c2cca250d104958fba898039b88cbbb7a426f
Glenn Lanzer <lanzertrey@gmail.com>
2019-03-28 09:04:03 -0500

    Add (php/tuts/learn-php): variables-and-types.php
    Added variables and types tutorial from learn-php.org

---
+++ computer-science/php/tutorials/learn-php.org/variables-and-types/variables-and-types-01.php
@@ -0,0 +1,15 @@
+<?php
+// Part 1: add the name and age variables:
+$name = "Jake";
+$age  = 20;
+
+echo "Hello $name. You are $age years old.\n";
+
+// Part 2: sum up the variables x and y and
+// put the result in the sum variable:
+$x = 195793;
+$y = 256836;
+$sum = $x + $y;
+
+echo "The sum of $x and $y is $sum.";
+?>
