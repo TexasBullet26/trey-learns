10292f10ffa2272d675fb53603f725fecbec0bf9
Glenn Lanzer <lanzertrey@gmail.com>
2019-04-18 20:17:18 -0500

    Add (react-landing-page): Footer component
    Includes:
    -------------
    
    src/components/Footer/index.js
    
      - Footer component index
    
    src/components/Footer/Footer.js
    
      - Footer component layout
    
    src/components/Footer/style.scss
    
      - Footer component styles

---
+++ computer-science/javascript/react/react-landing-page/src/components/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './style';
+
+const Footer = (props, context) => {
+    const { theme: { colorPrimary, textPrimary } } = context;
+    return (
+        <footer>
+            <div className='content-grid'>
+                <div style={{ color: textPrimary }}>
+                    Made by{' '}
+                    <a
+                        href='https://github.com/TexasBullet26'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        style={{ color: colorPrimary }}
+                    >
+                        GIII
+                    </a>{' '}
+                    <span role='img' aria-label='heart'>
+                        ❤️
+                    </span>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+Footer.contextTypes = {
+    theme: PropTypes.any,
+    switchTheme: PropTypes.func
+};
+
+export default Footer;
