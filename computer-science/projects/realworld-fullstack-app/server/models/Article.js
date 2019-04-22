da21c83bdfeeb17f917d308c303c289886723b3e
Glenn Lanzer <lanzertrey@gmail.com>
2019-04-22 17:28:59 -0500

    Add (feature): favoriting

--- computer-science/projects/realworld-fullstack-app/server/models/Article.js
+++ computer-science/projects/realworld-fullstack-app/server/models/Article.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 var slug = require('slug');
+var User = mongoose.model('User');
 
 var ArticleSchema = new mongoose.Schema({
     slug: {
@@ -36,6 +37,16 @@ ArticleSchema.methods.slugify = function() {
     this.slug = slug(this.title);
 };
 
+ArticleSchema.methods.updateFavoriteCount = function() {
+    var article = this;
+
+    return User.count({favorites: {$in: [article._id]}}).then(function(count) {
+        article.favoritesCount = count;
+
+        return article.save();
+    });
+};
+
 ArticleSchema.methods.toJSONFor = function(user) {
     return {
         slug: this.slug,
@@ -44,6 +55,8 @@ ArticleSchema.methods.toJSONFor = function(user) {
         body: this.body,
         createdAt: this.createdAt,
         updatedAt: this.updatedAt,
+        tagList: this.tagList,
+        favorited: user ? user.isFavorite(this._id) : false,
         favoritesCount: this.favoritesCount,
         author: this.author.toProfileJSONFor(user)
     };
