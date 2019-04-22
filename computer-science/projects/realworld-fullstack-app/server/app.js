067b570adcff6d1dfed8a97a7d2ee96a39a56991
Glenn Lanzer <lanzertrey@gmail.com>
2019-04-22 11:56:06 -0500

    Init (server): setup

--- computer-science/projects/realworld-fullstack-app/server/app.js
+++ computer-science/projects/realworld-fullstack-app/server/app.js
@@ -1,20 +1,97 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var fs = require('fs'),
+    http = require('http'),
+    path = require('path'),
+    methods = require('methods'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cors = require('cors'),
+    passport = require('passport'),
+    errorhandler = require('errorhandler'),
+    mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var isProduction = process.env.NODE_ENV === 'production';
 
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// Create global `app` object:
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Normal express config defaults:
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-module.exports = app;
+app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
+
+app.use(
+    session({
+        secret: 'conduit',
+        cookie: {
+            maxAge: 60000
+        },
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+if (!isProduction) {
+    app.use(errorhandler());
+}
+
+if (idProduction) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect('mongodb://localhost/conduit');
+    mongoose.set('debug', true);
+}
+
+app.use(require('./routes'));
+
+// catch 404 and forward to error handler:
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace:
+if (!isProduction) {
+    app.use(function(err, req, res, next) {
+        console.log(err.stack);
+
+        res.status(err.status || 500);
+
+        res.json({'errors': {
+            message: err.message,
+            error: err
+        }});
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user:
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({'errors': {
+        message: err.message,
+        error: {}
+    }});
+});
+
+// let's start our server...
+var server = app.listen( process.env.PORT || 3000, function() {
+    console.log('Listening on port ' + server.address().port);
+});
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// module.exports = app;
--- computer-science/projects/realworld-fullstack-app/server/routes/index.js
+++ computer-science/projects/realworld-fullstack-app/server/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
+
+router.use('/api', require('./api'));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
