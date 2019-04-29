34cc8383fae73945d43e25e0f08b9384e33abd70
Glenn Lanzer <lanzertrey@gmail.com>
2019-04-29 16:47:04 -0500

    Add (fcc-react): add inline styles in react
    Includes:
    -----------------------
    
    - 03-front-end-libraries/03.04-react/03.04.38-add-inline-styles-in-react

---
+++ computer-science/tutorials/freeCodeCamp/03-front-end-libraries/03.04-react/03.04.38-add-inline-styles-in-react/add-inline-styles-in-react.jsx
@@ -0,0 +1,13 @@
+const styles = {
+    color: "purple",
+    fontSize: 40,
+    border: "2px solid purple"
+}
+
+class Colorful extends React.Component {
+    render() {
+        return (
+            <div style={styles}>Style</div>
+        );
+    }
+};
