a38dc1669b6e0a837b141bfa6002eaf6b8d46ed8
Glenn Lanzer <lanzertrey@gmail.com>
2019-05-05 20:42:37 -0500

    Add (02.03-regular-expressions): using-the-test-method.js

---
+++ computer-science/tutorials/freeCodeCamp/02-javascript-algorithms-and-data-structures/02.03-regular-expressions/02.03.01-using-the-test-method/using-the-test-method.js
@@ -0,0 +1,4 @@
+let myString = 'Hello, World!';
+let myRegex = /Hello/;
+// Apply the regex `myRegex` on the string `myString` using the `.test()` method:
+let result = myRegex.test(myString); // Returns true
